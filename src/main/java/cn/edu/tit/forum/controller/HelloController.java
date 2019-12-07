107d652b706ba50a7130374c2ba372cbb643fedd
lichuangbo <15503609389@163.com>
2019-12-07 15:17:02 +0800

    init

---
+++ src/main/java/cn/edu/tit/forum/controller/HelloController.java
@@ -0,0 +1,21 @@
+package cn.edu.tit.forum.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author lichuangbo
+ * @version 1.0
+ * @created 2019/12/7
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
