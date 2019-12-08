3e38c35d466088f70f0eac380e1e9c2c156893f2
lichuangbo <15503609389@163.com>
2019-12-08 17:35:07 +0800

    add cookies

--- src/main/java/cn/edu/tit/forum/mapper/UserMapper.java
+++ src/main/java/cn/edu/tit/forum/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package cn.edu.tit.forum.mapper;
 import cn.edu.tit.forum.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * @author lichuangbo
@@ -14,4 +16,7 @@ public interface UserMapper {
     @Insert("insert into user(name, account_id, token, gmt_create, gmt_modified) " +
             "values(#{name}, #{accountId}, #{token}, #{gmtCreate}, #{gmtModified})")
     void insertUser(User user);
+
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
