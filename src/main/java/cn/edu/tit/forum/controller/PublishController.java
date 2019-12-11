3c30e91c1b2aee842c7bfe2e2462c132e8c7fd21
lichuangbo <15503609389@163.com>
2019-12-11 12:18:15 +0800

    edit question

--- src/main/java/cn/edu/tit/forum/controller/PublishController.java
+++ src/main/java/cn/edu/tit/forum/controller/PublishController.java
@@ -1,12 +1,14 @@
 package cn.edu.tit.forum.controller;
 
-import cn.edu.tit.forum.mapper.QuestionMapper;
+import cn.edu.tit.forum.dto.QuestionDTO;
 import cn.edu.tit.forum.model.Question;
 import cn.edu.tit.forum.model.User;
+import cn.edu.tit.forum.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -21,7 +23,7 @@ import javax.servlet.http.HttpServletRequest;
 public class PublishController {
 
     @Autowired
-    private QuestionMapper questionMapper;
+    private QuestionService questionService;
 
     @GetMapping("/publish")
     public String publish() {
@@ -32,6 +34,7 @@ public class PublishController {
     public String doPublish(@RequestParam("title") String title,
                             @RequestParam("description") String description,
                             @RequestParam("tag") String tag,
+                            @RequestParam("id") Integer id,
                             HttpServletRequest request,
                             Model model) {
         // 表单值回显（用户写入一部分，就点击提交，会得到不合法信息，同时保留了先前填入的值）
@@ -64,9 +67,19 @@ public class PublishController {
         question.setTitle(title);
         question.setTag(tag);
         question.setCreater(user.getId());
-        question.setGmtCreate(System.currentTimeMillis());
-        question.setGmtModified(question.getGmtCreate());
-        questionMapper.create(question);
+        question.setId(id);
+        questionService.createOrUpdate(question);
         return "redirect:/";
+    }
+
+    @GetMapping("/publish/{id}")
+    public String edit(@PathVariable(name = "id") Integer id,
+                       Model model) {
+        QuestionDTO question = questionService.getById(id);
+        model.addAttribute("title", question.getTitle());
+        model.addAttribute("description", question.getDescription());
+        model.addAttribute("tag", question.getTag());
+        model.addAttribute("id", question.getId());
+        return "publish";
     }
 }
